2bf9f01d0a610efe3c8633af241daf5478868a97
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-28 19:06:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,15 +2,17 @@ require 'pry'
 
 class Dog 
   
-  def name=(dog_name)
-    this_dogs_name = dog_name
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
+  
   def name
-    this_dogs_name
+    @this_dogs_name
   end
 end
 
 lassie = Dog.new
 lassie.name = "Lassie"
 
-binding.pry
+puts lassie.name
+
