1d5300bac5ce0339382cbb07de5ec73233c62d1d
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-28 19:03:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,16 @@
+require 'pry'
+
 class Dog 
   
-  def name 
-    @name
+  def name=(dog_name)
+    this_dogs_name = dog_name
   end
-  
+  def name
+    this_dogs_name
+  end
 end
 
-lassie = Dog.new 
+lassie = Dog.new
+lassie.name = "Lassie"
+
+binding.pry
